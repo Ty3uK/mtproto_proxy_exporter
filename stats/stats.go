40e3aaed3d32522705405cce5ba1de1c9ad51c0b
Maksim Karelov <max.karelov@gmail.com>
2018-10-20 12:05:54 +0300

    Comment stats.go exported members

--- stats/stats.go
+++ stats/stats.go
@@ -1,44 +1,47 @@
 package stats
 
 import (
-  "io/ioutil"
-  "net/http"
-  "strconv"
-  "strings"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
+// Stats represents mtproto_proxy stats data
 type Stats struct {
-  data []string
+	data []string
 }
 
+// GetData obtains mtproto_proxy stats data
 func (stats *Stats) GetData(url string) {
-  response, err := http.Get(url)
+	response, err := http.Get(url)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  defer response.Body.Close()
+	defer response.Body.Close()
 
-  body, err := ioutil.ReadAll(response.Body)
+	body, err := ioutil.ReadAll(response.Body)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  stats.data = strings.Split(string(body), "\n")
+	stats.data = strings.Split(string(body), "\n")
 }
 
+// GetNumberItem finds item in mtproto_proxy stats data
 func (stats *Stats) GetNumberItem(name string) (data float64) {
-  for _, statItem := range stats.data {
-    if strings.HasPrefix(statItem, name) {
-      if count, err := strconv.ParseFloat(strings.Split(statItem, "\t")[1], 64); err == nil {
-        return count
-      }
+	for _, statItem := range stats.data {
+		if strings.HasPrefix(statItem, name) {
+			if count, err := strconv.ParseFloat(strings.Split(statItem, "\t")[1], 64); err == nil {
+				return count
+			}
 
-      return -1
-    }
-  }
+			return -1
+		}
+	}
 
-  return -1
+	return -1
 }
