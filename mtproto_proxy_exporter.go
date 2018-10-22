8f29d2e53734def0b45c95cdc5bb61bb6a28b125
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2018-10-22 13:38:27 -0400

    run go fmt

--- mtproto_proxy_exporter.go
+++ mtproto_proxy_exporter.go
@@ -1,74 +1,74 @@
 package main
 
 import (
-  "flag"
-  "log"
-  "net/http"
-  "time"
+	"flag"
+	"log"
+	"net/http"
+	"time"
 
-  "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
-  configPkg "github.com/Ty3uK/mtproto_proxy_exporter/config"
-  metricsPkg "github.com/Ty3uK/mtproto_proxy_exporter/metrics"
-  statsPkg "github.com/Ty3uK/mtproto_proxy_exporter/stats"
+	configPkg "github.com/Ty3uK/mtproto_proxy_exporter/config"
+	metricsPkg "github.com/Ty3uK/mtproto_proxy_exporter/metrics"
+	statsPkg "github.com/Ty3uK/mtproto_proxy_exporter/stats"
 )
 
 var (
-  configPath = flag.String("config", "", "YML config path")
-  help       = flag.Bool("help", false, "prints help")
+	configPath = flag.String("config", "", "YML config path")
+	help       = flag.Bool("help", false, "prints help")
 
-  stats   = statsPkg.Stats{}
-  metrics = metricsPkg.Metrics{
-    List: make(map[string]metricsPkg.Item),
-  }
+	stats   = statsPkg.Stats{}
+	metrics = metricsPkg.Metrics{
+		List: make(map[string]metricsPkg.Item),
+	}
 )
 
 var config configPkg.Config
 
 func run() {
-  for {
-    stats.GetData(config.StatsAddress)
+	for {
+		stats.GetData(config.StatsAddress)
 
-    for _, item := range metrics.List {
-      item.SetValue(
-        stats.GetNumberItem(item.StatName),
-      )
-    }
+		for _, item := range metrics.List {
+			item.SetValue(
+				stats.GetNumberItem(item.StatName),
+			)
+		}
 
-    time.Sleep(time.Duration(config.Interval) * time.Second)
-  }
+		time.Sleep(time.Duration(config.Interval) * time.Second)
+	}
 }
 
 func initFromConfig() {
-  println("LISTENING ON  :", config.Address)
-  println("SCAN INTERVAL :", config.Interval)
-  println()
+	println("LISTENING ON  :", config.Address)
+	println("SCAN INTERVAL :", config.Interval)
+	println()
 
-  for _, configItem := range config.Metrics {
-    metrics.AddItem(
-      configItem.StatName,
-      configItem.Name,
-      configItem.Help,
-    )
+	for _, configItem := range config.Metrics {
+		metrics.AddItem(
+			configItem.StatName,
+			configItem.Name,
+			configItem.Help,
+		)
 
-    println("FROM MTPROTO  :", configItem.StatName)
-    println("TO PROMETHEUS :", configItem.Name)
-    println()
-  }
+		println("FROM MTPROTO  :", configItem.StatName)
+		println("TO PROMETHEUS :", configItem.Name)
+		println()
+	}
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  if *help {
-    configPkg.PrintHelp()
-    return
-  }
+	if *help {
+		configPkg.PrintHelp()
+		return
+	}
 
-  config = configPkg.InitFromFile(*configPath)
-  initFromConfig()
+	config = configPkg.InitFromFile(*configPath)
+	initFromConfig()
 
-  http.Handle("/metrics", promhttp.Handler())
-  go run()
-  log.Fatal(http.ListenAndServe(config.Address, nil))
+	http.Handle("/metrics", promhttp.Handler())
+	go run()
+	log.Fatal(http.ListenAndServe(config.Address, nil))
 }
